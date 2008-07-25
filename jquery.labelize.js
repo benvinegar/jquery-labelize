6e05c9639c88021c83a069aa3a06ebebba1e014a
fresh <fresh@ubuntu.localdomain>
2008-07-25 10:36:56 -0400

    Wasn't returning matched set; other fixes

--- jquery.labelize.js
+++ jquery.labelize.js
@@ -1,55 +1,43 @@
 /*
- * jQuery Labelize Plugin (jQuery >= 1.2.6)
- * 
- * This work is distributed under the MIT License:
- *   http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2008, Ben Vinegar [ ben ! benlog dot org ]
- * 
- * Usage:
- * 
- *   $('.myLabel').labelize()
- *   
- */
+* jQuery Labelize Plugin (jQuery >= 1.2.2)
+*
+* This work is distributed under the MIT License:
+* http://www.opensource.org/licenses/mit-license.php
+*
+* Copyright 2008, Ben Vinegar [ ben ! benlog dot org ]
+*
+* Usage:
+*
+* $('.myLabel').labelize()
+*
+*/
 jQuery.fn.labelize = function() {
   
   /* Apply cursor attribute to containers */
   $(this).css('cursor', 'pointer');
   
   /* Checkboxes */
-  if (!$.browser.msie) { /* For all browsers but IE */
-    var labelClickEvent = function() {
-      var checkbox = $('input', this);
-      var isChecked = !!checkbox.attr('checked');
-      
-      if (isChecked) checkbox.removeAttr('checked');
-      else           checkbox.attr('checked', true);
-      $(checkbox).triggerHandler('change');
-    }
-    
-    $(this).filter(':has(input[type=checkbox])').click(labelClickEvent);
-  
-    var containers = this;
-    $('input[type=checkbox]', this)
-      .click(function() {
-        $(containers).unbind('click', labelClickEvent);
-      })
-      .mouseout(function() { 
-        $(containers).click(labelClickEvent);
-    });
-    
-  } else {
-    $(this).filter(':has(input[type=checkbox])').click(function() {
-      var checkbox = $('input', this);
-      $(checkbox).click();
-      $(checkbox).triggerHandler('change');
-    });
+  var labelClickEvent = function() {
+
+    var checkbox = $('input', this);
+    checkbox.click();
   }
+    
+  $(this).filter(':has(input[type=checkbox])').click(labelClickEvent);
   
+  var containers = this;
+  $('input[type=checkbox]', this)
+    .mouseover(function() {
+      $(containers).unbind('click', labelClickEvent);
+    })
+    .mouseout(function() {
+      $(containers).click(labelClickEvent);
+  });
+    
   /* Radio Buttons */
   $(this).filter(':has(input[type=radio])').click(function() {
     $('input[name="' + $(this).attr('name') + '"]').removeAttr('checked');
     $('input', this).attr('checked', true);
   });
-  
+  return this;
 }
