62bd0581c418624109bbc351186cdd3e9df5f2f2
Ben Vinegar <ben@benlog.org>
2008-08-18 22:14:59 -0400

    No more special case code for radio buttons, some refactoring.

--- jquery.labelize.js
+++ jquery.labelize.js
@@ -1,43 +1,36 @@
 /*
-* jQuery Labelize Plugin (jQuery >= 1.2.2)
-*
-* This work is distributed under the MIT License:
-* http://www.opensource.org/licenses/mit-license.php
-*
-* Copyright 2008, Ben Vinegar [ ben ! benlog dot org ]
-*
-* Usage:
-*
-* $('.myLabel').labelize()
-*
-*/
+ * jQuery Labelize Plugin (jQuery >= 1.2.2)
+ *
+ * This work is distributed under the MIT License:
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2008, Ben Vinegar [ ben ! benlog dot org ]
+ *
+ * Usage:
+ *
+ * $('.myLabel').labelize()
+ *
+ */
+
 jQuery.fn.labelize = function() {
   
   /* Apply cursor attribute to containers */
   $(this).css('cursor', 'pointer');
-  
-  /* Checkboxes */
-  var labelClickEvent = function() {
-
-    var checkbox = $('input', this);
-    checkbox.click();
-  }
     
-  $(this).filter(':has(input[type=checkbox])').click(labelClickEvent);
+  $(this).filter(':has(input)').click(jQuery.fn.labelize.labelClickEvent);
   
   var containers = this;
-  $('input[type=checkbox]', this)
+  $('input', this)
     .mouseover(function() {
-      $(containers).unbind('click', labelClickEvent);
+      $(containers).unbind('click', jQuery.fn.labelize.labelClickEvent);
     })
     .mouseout(function() {
-      $(containers).click(labelClickEvent);
+      $(containers).click(jQuery.fn.labelize.labelClickEvent);
   });
     
-  /* Radio Buttons */
-  $(this).filter(':has(input[type=radio])').click(function() {
-    $('input[name="' + $(this).attr('name') + '"]').removeAttr('checked');
-    $('input', this).attr('checked', true);
-  });
   return this;
+}
+
+jQuery.fn.labelize.labelClickEvent = function() {
+  $('input', this).click();
 }
