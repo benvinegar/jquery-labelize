78f8a4bf59b49520299cb37c054bb6a818231471
bentlegen <ben@benlog.org>
2009-01-24 12:20:36 -0500

    Wrap in a closure (cherry picked from commit 4410c9a69b008d3620bf6cad82855e3a501c90ae)
    Conflicts:
    
    	jquery.labelize.js

--- jquery.labelize.js
+++ jquery.labelize.js
@@ -1,48 +1,50 @@
 /*
- * jQuery Labelize Plugin (jQuery >= 1.2.2)
- *
- * This work is distributed under the MIT License:
- * http://www.opensource.org/licenses/mit-license.php
- *
- * Copyright 2008, Ben Vinegar [ ben ! benlog dot org ]
- *
- * Usage:
- *
- * $('.myLabel').labelize()
- *
- */
-
-jQuery.fn.labelize = function(hoverClass) {
-
-  var containers = $(this).filter(':has(input)');
+* jQuery Labelize Plugin (jQuery >= 1.2.2)
+*
+* This work is distributed under the MIT License:
+* http://www.opensource.org/licenses/mit-license.php
+*
+* Copyright 2008, Ben Vinegar [ ben ! benlog dot org ]
+*
+* Usage:
+*
+* $('.myLabel').labelize()
+*
+*/
+ 
+(function($) {
+  $.fn.labelize = function(hoverClass) {
   
-  // Apply cursor attribute to containers
-  $(containers).css('cursor', 'pointer');
-  
-  // Apply click event to container
-  containers.click(jQuery.fn.labelize.labelClickEvent);
-  
-  // Apply optional hoverClass
-  if (hoverClass) {
-    containers
-      .mouseover(function() { $(this).addClass(hoverClass) })
-      .mouseout (function() { $(this).removeClass(hoverClass) });
+    var containers = $(this).filter(':has(input)');
+    
+    // Apply cursor attribute to containers
+    $(containers).css('cursor', 'pointer');
+    
+    // Apply click event to container
+    containers.click($.fn.labelize.labelClickEvent);
+    
+    // Apply optional hoverClass
+    if (hoverClass) {
+      containers
+        .mouseover(function() { $(this).addClass(hoverClass) })
+        .mouseout (function() { $(this).removeClass(hoverClass) });
+    }
+    
+    // Remove encompassing label event when hovering over the
+    // input element; this makes sure click() events don't fire twice
+    $('input', this)
+      .mouseover(function() {
+        $(containers).unbind('click', $.fn.labelize.labelClickEvent);
+      })
+      .mouseout(function() {
+        $(containers).click($.fn.labelize.labelClickEvent);
+    });
+      
+    return this;
   }
   
-  // Remove encompassing label event when hovering over the
-  // input element; this makes sure click() events don't fire twice
-  $('input', this)
-    .mouseover(function() {
-      $(containers).unbind('click', jQuery.fn.labelize.labelClickEvent);
-    })
-    .mouseout(function() {
-      $(containers).click(jQuery.fn.labelize.labelClickEvent);
-  });
-    
-  return this;
-}
-
-jQuery.fn.labelize.labelClickEvent = function() {
-  $('input', this).click();
-}
-
+  $.fn.labelize.labelClickEvent = function() {
+    $('input', this).click();
+  }
+  
+})(jQuery);
