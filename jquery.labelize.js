7e134251a92e63e42bc3a05b257d3081b52d3fe8
bentlegen <ben@benlog.org>
2010-01-24 15:00:48 -0500

    wrong scope for labelClickEvent(), plus some formatting fixes

--- jquery.labelize.js
+++ jquery.labelize.js
@@ -12,59 +12,54 @@
  *
  */
 
-(function($) { 
-  $.fn.labelize = function(hoverClass) {
-  
-    function labelClickEvent() {
-      // remove encompassing event (prevents jQuery recursion error in 1.3.x)
-      $(this).unbind('click', labelClickEvent); 
-      
-      // call .click on owned input
-      $input = $(this).find('input');
-      
-      if ($input.is(':checked')) {
-        $input.removeAttr('checked').change();
-      } else {
-        $input.attr('checked', 'checked').change();
-      }
-      
-      // re-apply the event after we're done
-      $(this).click(labelClickEvent);
+(function($) {
+  function labelClickEvent() {
+    // remove encompassing event (prevents jQuery recursion error in 1.3.x)
+    $(this).unbind('click', labelClickEvent);
+
+    // call .click on owned input
+    $input = $(this).find('input');
+
+    if ($input.is(':checked')) {
+      $input.removeAttr('checked').change();
+    } else {
+      $input.attr('checked', 'checked').change();
     }
-    
+
+    // re-apply the event after we're done
+    $(this).click(labelClickEvent);
+  }
+
+  $.fn.labelize = function(hoverClass) {
+
     var containers = $(this).filter(':has(input)');
-    
+
     // Apply cursor attribute and onclick event to containers
     $(containers)
       .css('cursor', 'pointer')
       .click(labelClickEvent);
-    
+
     // Apply optional hoverClass
-    if (hoverClass) {
+    if (typeof hoverClass === 'string') {
       containers
         .mouseover(function() { $(this).addClass(hoverClass) })
         .mouseout (function() { $(this).removeClass(hoverClass) });
     }
-    
+
     // Remove encompassing label event when hovering over the
     // input element; this makes sure click() events don't fire twice
     $('input', this)
-      .mouseover(function() {
-        $(containers).unbind('click', labelClickEvent);
-      })
-      .mouseout(function() {
-        $(containers).click(labelClickEvent);
-    }).focus(function() {
-        $(containers).unbind('click', labelClickEvent);
-    }).blur(function() {
-        $(containers).click(labelClickEvent);
-    }).click(function() {
-        if($(this).is('[type=checkbox], [type=radio]') && $.browser.msie) {
+      .mouseover(function() { $(containers).unbind('click', labelClickEvent); })
+      .mouseout(function()  { $(containers).click(labelClickEvent); })
+      .focus(function()     { $(containers).unbind('click', labelClickEvent); })
+      .blur(function()      { $(containers).click(labelClickEvent); })
+      .click(function() {
+        if ($(this).is('[type=checkbox], [type=radio]') && $.browser.msie) {
           $(this).change();
         }
     });
-      
+
     return this;
   }
-  
+
 })(jQuery);
